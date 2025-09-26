dbe1a50ed7cf4c0190b5fcf0ae9fba7a090cae0f
Yongtao Ye <53943996+ytye2010@users.noreply.github.com>
2025-09-26 17:43:06 +0800

    Add files via upload

---
+++ Scripts/RNA_secondary_structure/RNAfold_script.cs
@@ -0,0 +1,430 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RNAfold_script
+{
+    class RNAfold_script
+    {
+        static void Main_RNAfold_extract(string[] args) //_RNAfold_extract
+        {
+            string rnafold_outfile = args[0]; ///data/ytye/influenza/RNAFold/rnafold/HA_rnafold.txt
+            Dictionary<string, string> seqnanme2seq = new Dictionary<string, string>();
+            Dictionary<string, List<double>> seqnanme2info = new Dictionary<string, List<double>>();
+            int i = 0;
+            List<String[]> ls = ReadSpace(rnafold_outfile);
+            while (i < ls.Count)
+            {
+                if (ls[i][0][0] == '>') //header
+                {
+                    string seqname = ls[i][0];
+                    seqnanme2seq.Add(seqname, ls[i + 1][0]);
+                    double mfe = Double.Parse(ls[i + 2][1].Substring(1, ls[i + 2][1].Length - 2));
+                    double collecttime = Double.Parse(seqname.Split('|')[3]);
+                    List<double> temp = new List<double>();
+                    temp.Add(collecttime);
+                    temp.Add(mfe);
+                    seqnanme2info.Add(seqname, temp);
+                }
+                i++;
+            }
+            Console.WriteLine(seqnanme2info.Count);
+            Parallel.ForEach(seqnanme2seq, item =>
+            {
+                string seqname = item.Key;
+                int[] res = getGCCountAndLength_exact(item.Value);
+                double gccontent = res[0] / (double)res[1] * 100;
+                seqnanme2info[seqname].Add(gccontent);
+                seqnanme2info[seqname].Add(res[1]); //length ATCG
+                seqnanme2info[seqname].Add(item.Value.Length);
+
+                res = getGC3CountAndLength_exact(item.Value);
+                double gc3 = res[0] / (double)res[1] * 100;
+                seqnanme2info[seqname].Add(gc3);
+
+                res = getGC12CountAndLength_exact(item.Value);
+                double gc12 = res[0] / (double)res[1] * 100;
+                seqnanme2info[seqname].Add(gc12);
+
+            });
+            string rnafold_statics_file = rnafold_outfile.Replace(".txt", "") + "_statics.csv";
+            StreamWriter fileWriter = new StreamWriter(rnafold_statics_file, false, Encoding.ASCII);
+            foreach (var item in seqnanme2info)
+            {
+                fileWriter.Write(item.Key.Substring(1));
+                for (int j = 0; j < item.Value.Count; ++j)
+                    fileWriter.Write("," + item.Value[j]);
+                fileWriter.Write("\n");
+            }
+            fileWriter.Close();
+        }
+
+        static void Main_linage(string[] args)
+        {
+            string rnafold_statics_file = args[0];
+            List<String[]> ls = ReadCSV(rnafold_statics_file);
+            Dictionary<string, String[]> seqnanme2info = new Dictionary<string, String[]>();
+            for (int i = 0; i < ls.Count; i++)
+            {
+                seqnanme2info.Add(ls[i][0], ls[i]);
+            }
+            Console.WriteLine(ls.Count);
+            string lineagefile = args[1];
+            List<String[]> gdata = ReadTSV(lineagefile);
+            Console.WriteLine(gdata.Count);
+
+            string[] usedlineage = { "Hu1", "Hu2", "Hu3", "Hu4", "Eq", "Ca1", "Ca2", "Sw1", "Sw2", "Sw3", "Sw4", "Sw5" };
+
+            HashSet<string> usedlineageSet = new HashSet<string>(usedlineage);
+
+
+            Dictionary<string, List<String>> lineageAll = new Dictionary<string, List<String>>();
+            for (int i = 0; i < gdata.Count; ++i)
+            {
+                string lineage = gdata[i][0];
+                if (usedlineageSet.Contains(lineage))
+                {
+                    lineageAll.Add(lineage, new List<String>());
+                }
+               
+                for (int j = 1; j < gdata[i].Length; ++j)
+                {
+                    lineageAll[lineage].Add(gdata[i][j]);
+                }
+            }
+            string outfilePrefix = rnafold_statics_file.Replace(".txt", "");
+            outfilePrefix = outfilePrefix.Replace(".csv", "");
+            outfilePrefix = "lineage/" + outfilePrefix;
+            foreach (var item in lineageAll)
+            {
+                string lineage = item.Key;
+                string outfile = outfilePrefix + "_" + lineage + ".csv";
+                StreamWriter fileWriter = new StreamWriter(outfile, false, Encoding.ASCII);
+                for (int j = 0; j < item.Value.Count; ++j)
+                {
+                    string seqname = item.Value[j];
+                    if (!seqnanme2info.ContainsKey(seqname))
+                    {
+                        Console.WriteLine("error can not find " + seqname + " in " + rnafold_statics_file);
+                        continue;
+                    }
+                    fileWriter.WriteLine(string.Join(",", seqnanme2info[seqname]));
+                }
+                fileWriter.Close();
+            }
+        }
+
+        static void Main_summary(string[] args)
+        {
+            string rnafold_statics_file = args[0];
+            List<String[]> ls = ReadCSV(rnafold_statics_file);
+            Dictionary<string, String[]> seqnanme2info = new Dictionary<string, String[]>();
+            for (int i = 0; i < ls.Count; i++)
+            {
+                seqnanme2info.Add(ls[i][0], ls[i]);
+            }
+            Console.WriteLine(ls.Count);
+            string lineagefile = args[1];
+            List<String[]> gdata = ReadTSV(lineagefile);
+            Console.WriteLine(gdata.Count);
+
+            string[] mammal_lineage = { "Hu1", "Hu2", "Hu3", "Hu4", "Eq", "Ca1", "Ca2", "Sw1", "Sw2", "Sw3", "Sw4", "Sw5" };
+
+            HashSet<string> mammallineageSet = new HashSet<string>(mammal_lineage);
+
+            Dictionary<string, List<String>> lineageAll = new Dictionary<string, List<String>>();
+            for (int i = 0; i < gdata.Count; ++i)
+            {
+                string lineage = gdata[i][0];
+                else if (mammallineageSet.Contains(lineage)) lineage = "Ma";
+                else if (lineage != "Av") lineage = "Sp";
+                if (!lineageAll.ContainsKey(lineage)) lineageAll.Add(lineage, new List<String>());
+                for (int j = 1; j < gdata[i].Length; ++j)
+                {
+                    lineageAll[lineage].Add(gdata[i][j]);
+                }
+            }
+            string outfilePrefix = rnafold_statics_file.Replace(".txt", "");
+            outfilePrefix = outfilePrefix.Replace(".csv", "");
+            outfilePrefix = "lineage/" + outfilePrefix;
+            string outfile = outfilePrefix + "_MFE_GC.csv";
+            StreamWriter fileWriter = new StreamWriter(outfile, false, Encoding.ASCII);
+            fileWriter.WriteLine("lineage,seqname,collectiontime,mfe,gc,atcgSeqL,seqL");
+            foreach (var item in lineageAll)
+            {
+                string lineage = item.Key;
+                for (int j = 0; j < item.Value.Count; ++j)
+                {
+                    string seqname = item.Value[j];
+                    if (!seqnanme2info.ContainsKey(seqname))
+                    {
+                        Console.WriteLine("error can not find " + seqname + " in " + rnafold_statics_file);
+                        continue;
+                    }
+                    fileWriter.WriteLine(lineage + "," + string.Join(",", seqnanme2info[seqname]));
+                }
+            }
+            fileWriter.Close();
+        }
+
+        //add GC3 and GC12
+        static void Main_add_GC3_GC12(string[] args) //add_GC3_GC12
+        {
+            string gene = args[0]; //HA
+            //read msa
+            string msafile = "/data/ytye/influenza/RNAFold/pal2nal/" + gene + "_nt_all.fasta";
+            Dictionary<string, string> seqnanme2seq = ReadMSA(msafile);
+
+            //read lineage, mfe and others
+            string[] lineages = { "Av", "Ca1", "Ca2", "Eq", "Hu1", "Hu2", "Hu3", "Hu4", "Sp", "Sw1", "Sw2", "Sw3", "Sw4", "Sw5" };
+            string prefixPath = "/data/ytye/influenza/RNAFold/lineage/";
+            for (int i = 0; i < lineages.Length; ++i)
+            {
+                string mfefile = prefixPath + gene + "_rnafold_statics_" + lineages[i] + ".csv";
+                List<String[]> ls = ReadCSV(mfefile);
+                string rnafold_statics_file = prefixPath + gene + "_rnafold_addGC3_12_statics_" + lineages[i] + ".csv";
+                StreamWriter fileWriter = new StreamWriter(rnafold_statics_file, false, Encoding.ASCII);
+
+                for (int j = 0; j < ls.Count; ++j)
+                {
+                    string seqname = ">" + ls[j][0];
+                    if (!seqnanme2seq.ContainsKey(seqname))
+                        Console.WriteLine("can not find " + seqname + " in " + gene);
+                    string seq = seqnanme2seq[seqname];
+                    int[] res = getGC3CountAndLength_exact(seq);
+                    double gc3 = res[0] / (double)res[1] * 100;
+                    res = getGC12CountAndLength_exact(seq);
+                    double gc12 = res[0] / (double)res[1] * 100;
+                    //res = getGCCountAndLength_exact(seq);
+                    //double gc = res[0] / (double)res[1] * 100;
+                    fileWriter.WriteLine(string.Join(",", ls[j]) + "," + gc3 + "," + gc12);
+                }
+                fileWriter.Close();
+            }
+        }
+
+        static void Main_lineageSeq(string[] args) //get lineage sequences
+        {
+            string[] genes = { "PB2", "PB1", "PA", "HA", "NP", "NA", "M1", "NS1" };
+            Dictionary<string, Dictionary<string, string>> allseqnanme2seq = new Dictionary<string, Dictionary<string, string>>();
+            for (int k = 0; k < 8; ++k)
+            {
+                string gene = genes[k];
+                string msafile = "/data/ytye/influenza/RNAFold/pal2nal/" + gene + "_nt_all.fasta";
+                Dictionary<string, string> seqnanme2seq = ReadMSA(msafile);
+                allseqnanme2seq.Add(gene, seqnanme2seq);
+            }
+
+            //read lineage, mfe and others
+            string[] lineages = { "Ca1", "Ca2", "Eq", "Hu1", "Hu2", "Hu3", "Hu4", "Sw1", "Sw2", "Sw3", "Sw4", "Sw5"};
+            string prefixPath = "/data/ytye/influenza/RNAFold/lineage/";
+
+            for (int i = 0; i < lineages.Length; ++i)
+            {
+                Dictionary<string, string> sequences = new Dictionary<string, string>();
+                Dictionary<string, int> sequence2gene = new Dictionary<string, int>();
+                string mfefile = prefixPath + "HA_rnafold_statics_" + lineages[i] + ".csv";
+                List<String[]> ls = ReadCSV(mfefile);
+                for (int k = 0; k < 8; ++k)
+                {
+                    string gene = genes[k];
+                    Dictionary<string, string> seqnanme2seq = allseqnanme2seq[gene];
+                    for (int j = 0; j < ls.Count; ++j)
+                    {
+                        string seqname = ">" + ls[j][0];
+                        string seq = seqnanme2seq[seqname];
+                        if (!sequences.ContainsKey(seqname))
+                        {
+                            sequences.Add(seqname, "");
+                            sequence2gene.Add(seqname, 0);
+                        }
+                        sequences[seqname] += seq;
+                        sequence2gene[seqname]++;
+                    }
+                }
+                string outfile = "/data/ytye/influenza/codon_bias/lineage_seq/" + lineages[i] + "_genome.fas";
+                StreamWriter fileWriter = new StreamWriter(outfile, false, Encoding.ASCII);
+                foreach (var item in sequences)
+                {
+                    if (sequence2gene[item.Key] == 8)
+                    {
+                        fileWriter.WriteLine(item.Key);
+                        fileWriter.WriteLine(item.Value);
+                    }
+                    else
+                    {
+                        Console.WriteLine("can not find " + item.Key);
+                    }
+                }
+                fileWriter.Close();
+            }
+        }
+
+
+        static void Main(string[] args) //_split_time
+        { 
+            //read lineage, mfe and others
+            string[] lineages = { "Ca1", "Ca2", "Eq", "Hu1", "Hu2", "Hu3", "Hu4", "Sw1", "Sw2", "Sw3", "Sw4", "Sw5" };
+            string prefixPath = @"D:\work\projects\Influenza evolution\codon_bias\";
+            for (int i = 0; i < lineages.Length; ++i)
+            {
+                string caifile = prefixPath +  "cai_" + lineages[i] + ".csv";
+                List<String[]> ls = ReadCSV(caifile);
+                string outfile = prefixPath + lineages[i] + "_cai_time" + ".csv";
+
+                StreamWriter fileWriter = new StreamWriter(outfile, false, Encoding.ASCII);
+
+                for (int j = 0; j < ls.Count; ++j)
+                {
+                    string seqname =ls[j][0];
+                    string[] arr = seqname.Split('|');
+                    fileWriter.WriteLine(string.Join(",", ls[j]) + "," + arr[3]);
+                }
+                fileWriter.Close();
+            }
+        }
+
+
+        static int[] getGCCountAndLength_exact(string seq)
+        {
+            int count = 0;
+            int totalL = 0;
+            int IUPAC = 0;
+            for (int i = 0; i < seq.Length; ++i)
+            {
+                if (seq[i] == 'G' || seq[i] == 'C') count++;
+                if (seq[i] == 'G' || seq[i] == 'C' || seq[i] == 'A' || seq[i] == 'T' || seq[i] == 'U') totalL++; //modificy on 20230329
+                else IUPAC++;
+            }
+            int[] res = new int[3];
+            res[0] = count;
+            res[1] = totalL;
+            res[2] = IUPAC;
+            return res;
+        }
+
+        static int[] getGC3CountAndLength_exact(string seq, bool excludeGap = true)
+        {
+            int count = 0;
+            int totalL = 0;
+            if (seq.Length % 3 != 0)
+            {
+                Console.WriteLine("seqlength error = " + seq.Length + "," + seq);
+            }
+            for (int i = 2; i < seq.Length; i += 3)
+            {
+                if (seq[i] == 'G' || seq[i] == 'C') count++;
+                if (seq[i] == 'G' || seq[i] == 'C' || seq[i] == 'A' || seq[i] == 'T' || seq[i] == 'U') totalL++;
+            }
+            int[] res = new int[2];
+            res[0] = count;
+            res[1] = totalL;
+            return res;
+        }
+
+        static int[] getGC12CountAndLength_exact(string seq)
+        {
+            int count = 0;
+            int totalL = 0;
+            if (seq.Length % 3 != 0)
+            {
+                Console.WriteLine("seqlength error = " + seq.Length + "," + seq);
+            }
+            for (int i = 0; i < seq.Length; i++)
+            {
+                if (i % 3 == 2) continue;
+                if (seq[i] == 'G' || seq[i] == 'C') count++;
+                if (seq[i] == 'G' || seq[i] == 'C' || seq[i] == 'A' || seq[i] == 'T' || seq[i] == 'U') totalL++;
+            }
+            int[] res = new int[2];
+            res[0] = count;
+            res[1] = totalL;
+            return res;
+        }
+
+        static List<String[]> ReadSpace(string filePathName)
+        {
+            List<String[]> ls = new List<String[]>();
+            StreamReader fileReader = new StreamReader(filePathName, Encoding.Default);
+            string line = "";
+            while ((line = fileReader.ReadLine()) != null)
+            {
+                line = line.Trim();
+                if (line.Length > 0)
+                {
+                    ls.Add(line.Split(' '));
+                }
+            }
+            fileReader.Close();
+            return ls;
+        }
+
+        static List<String[]> ReadTSV(string filePathName)
+        {
+            List<String[]> ls = new List<String[]>();
+            StreamReader fileReader = new StreamReader(filePathName, Encoding.Default);
+            string line = "";
+            while ((line = fileReader.ReadLine()) != null)
+            {
+                line = line.Trim();
+                if (line.Length > 0)
+                {
+                    ls.Add(line.Split('\t'));
+                }
+            }
+            fileReader.Close();
+            return ls;
+        }
+
+        static List<String[]> ReadCSV(string filePathName)
+        {
+            List<String[]> ls = new List<String[]>();
+            StreamReader fileReader = new StreamReader(filePathName, Encoding.Default);
+            string line = "";
+            while ((line = fileReader.ReadLine()) != null)
+            {
+                line = line.Trim();
+                if (line.Length > 0)
+                {
+                    ls.Add(line.Split(','));
+                }
+            }
+            fileReader.Close();
+            return ls;
+        }
+
+        static Dictionary<string, string> ReadMSA(string msafile, int minLength = 1, int maxLength = 10000000)
+        {
+            Dictionary<string, string> msaMap = new Dictionary<string, string>();
+            StreamReader fileReader = new StreamReader(msafile, Encoding.Default);
+            string line, seqName = "", seqLine = "";
+            while ((line = fileReader.ReadLine()) != null)
+            {
+                line = line.Trim();
+                if (line[0] == '>')
+                {
+                    if (seqLine.Length > minLength && seqLine.Length < maxLength && !msaMap.ContainsKey(seqName))
+                    {
+                        msaMap.Add(seqName, seqLine.ToUpper());
+                    }
+                    seqName = line;
+                    seqLine = "";
+                }
+                else
+                {
+                    seqLine += line;
+                }
+            }
+            if (seqLine.Length > minLength && seqLine.Length < maxLength && !msaMap.ContainsKey(seqName))
+            {
+                msaMap.Add(seqName, seqLine.ToUpper());
+            }
+            fileReader.Close();
+            return msaMap;
+        }
+
+    }
+}
--- Scripts/RNA_secondary_structure/mfe_gc_correlation.m
+++ Scripts/RNA_secondary_structure/mfe_gc_correlation.m
@@ -1,1 +1,194 @@
+clear all
+
+colors(3,:) = [105,105,105]/255; % dim grey
+
+filelist = {
+'_rnafold_addGC3_12_statics_Hu1.csv';
+'_rnafold_addGC3_12_statics_Hu2.csv';
+'_rnafold_addGC3_12_statics_Hu3.csv';
+'_rnafold_addGC3_12_statics_Hu4.csv';
+'_rnafold_addGC3_12_statics_Eq.csv';
+'_rnafold_addGC3_12_statics_Ca1.csv';
+'_rnafold_addGC3_12_statics_Ca2.csv';
+'_rnafold_addGC3_12_statics_Sw1.csv';
+'_rnafold_addGC3_12_statics_Sw2.csv';
+'_rnafold_addGC3_12_statics_Sw3.csv';
+'_rnafold_addGC3_12_statics_Sw4.csv';
+'_rnafold_addGC3_12_statics_Sw5.csv';
+%'_rnafold_statics_Sp.csv';
+%'_rnafold_statics_Av.csv';
+ };
+
+prefixPath = 'MFE/NS1/NS1';
+
+ % NS
+ ymin = -280;
+ ymax = -200;
+ xmin = 40;
+ xmax = 45;
+
+slope = zeros(size(filelist,1),1);
+pvalue = zeros(size(filelist,1),1);
+bints = zeros(size(filelist,1),2);
+R2 =  zeros(size(filelist,1),1);
+corr_value = zeros(size(filelist,1),1);
+corr_pvalue = zeros(size(filelist,1),1);
+counts = zeros(size(filelist,1),1);
+gc_content = zeros(size(filelist,1),1);
+gc3_content = zeros(size(filelist,1),1);
+gc3_corr_value = zeros(size(filelist,1),1);
+gc3_corr_pvalue = zeros(size(filelist,1),1);
+gc12_content = zeros(size(filelist,1),1);
+gc12_corr_value = zeros(size(filelist,1),1);
+gc12_corr_pvalue = zeros(size(filelist,1),1);
+mfe = zeros(size(filelist,1),1);
+std_gc = zeros(size(filelist,1),1);
+std_mfe = zeros(size(filelist,1),1);
+
+for i=1:size(filelist,1)
+    filepath = strcat(prefixPath,filelist{i,1});
+    alld = importdata(filepath);
+    d = alld.data;
+    %d = load(filepath);
+  
+     if i == 1
+         d = d(find(d(:,1)<2010), :);
+     end
+       
+     %d = d(find(d(:,5)==890), :);
+     
+     if(size(d,1) < 1) 
+         continue;
+     end
+    mysize = int2str(size(d,1));
+    
+    x = d(:,3); %GC
+    %x = d(:,1); %time
+    y = d(:,2); %GC
+    %y = d(:,2); %MFE
+
+    x_gc3 = d(:,6); %GC
+    x_gc12 = d(:,7); %GC
+
+    avgx = x;
+    avgy = y;
+
+    p=polyfit(avgx,avgy,1);  
+    X = [ones(size(avgx)), avgx];
+    [b,bint,r,rint,stats] = regress(avgy, X);
+        
+    x1=linspace(min(x),max(x));  
+    y1=polyval(p,x1);  
+    slope(i,1) = p(1,1);
+    pvalue(i,1) = stats(1,3);
+    bints(i,1) = bint(2,1);
+    bints(i,2) = bint(2,2);
+    R2(i,1) = stats(1,1);
+    [rho,pval] = corr(avgx,avgy);
+    corr_value(i,1) = rho;
+    corr_pvalue(i,1) = pval;
+    counts(i,1) = size(d,1);
+    gc_content(i,1) = mean(x);
+    mfe(i,1) = mean(y);
+    std_gc(i,1) = std(x);
+    std_mfe(i,1) = std(y);
+
+    [rho,pval] = corr(x_gc3,avgy);
+    gc3_corr_value(i,1) = rho;
+    gc3_corr_pvalue(i,1) = pval;
+
+    [rho,pval] = corr(x_gc12,avgy);
+    gc12_corr_value(i,1) = rho;
+    gc12_corr_pvalue(i,1) = pval;
+
+
+    %subplot(3, 4, i);
+
+    %plot(x,y,'.', 'Color',colors(3,:));
+    hold on
+     if i <= 12 && i >= 1
+     %plot(x1,y1, 'r');
+     end
+     
+     if i == 15
+        d1 = d(find(d(:,1)<=1958), :);
+        %d1 = d1(find(d1(:,1)>1918), :);
+        avgx = d1(:,3);
+        avgy = d1(:,2);
+        p=polyfit(avgx,avgy,1);  
+        X = [ones(size(avgx)), avgx];
+        [b,bint,r,rint,stats] = regress(avgy, X);
+        
+        x1=linspace(min( avgx),max(avgx));  
+        y1=polyval(p,x1);  
+        slope(i,1) = p(1,1);
+        pvalue(i,1) = stats(1,3);
+		bints(i,1) = bint(2,1);
+		bints(i,2) = bint(2,2);
+		R2(i,1) = stats(1,1);
+      
+		plot(x1,y1, 'r');
+      
+		hold on
+      
+		d2 = d(find(d(:,1)>1958), :);
+        avgx = d2(:,3);
+        avgy = d2(:,2);%*100;
+        p=polyfit(avgx,avgy,1);  
+        X = [ones(size(avgx)), avgx];
+        [b,bint,r,rint,stats] = regress(avgy, X);
+        
+        x1=linspace(min( avgx),max(avgx));  
+		y1=polyval(p,x1);  
+		slope(i,2) = p(1,1);
+		pvalue(i,2) = stats(1,3);
+		bints(i,3) = bint(2,1);
+		bints(i,4) = bint(2,2);
+		R2(i,2) = stats(1,1);
+      
+		%plot(x1,y1, 'r');
+
+    end
+     
+    titlename = strrep(filelist{i,1},'_rnafold_statics_','');
+    titlename = strrep(titlename,'.csv','');
+ 
+    titlename = strcat(titlename, ' (', mysize, ')');
+    title(titlename, 'FontSize', 7);  
+    
+%     xmin=floor(min(x(:,1)));
+%       xmax=ceil(max(x(:,1)));
+%         
+%       xlim([xmin,xmax]);
+%       interval =  (xmax-xmin)/2;
+%       set(gca,'XTick',[xmin:interval:xmax]);
+
+    set(gca,'xlim', [xmin, xmax], 'FontSize', 6);
+    set(gca,'ylim', [ymin, ymax], 'FontSize', 6);
+     
+     if i==1
+        %ylabel('GC content (%)', 'FontSize', 7);  
+        ylabel('MFE (kcal/mol)', 'FontSize', 7);  
+		xlabel('GC content (%)', 'FontSize', 7);  
+        %xlabel('Time', 'FontSize', 7);  
+     end
+end
+
+summary = zeros(size(filelist,1),5);
+summary(:,1) = slope(:,1);
+summary(:,2:3) = bints(:,1:2);
+summary(:,4) = pvalue(:,1);
+summary(:,5) =R2(:,1);
+summary(:,6) = corr_value(:,1);
+summary(:,7) = corr_pvalue(:,1);
+summary(:,8) = counts(:,1);
+summary(:,9) = gc_content(:,1);
+summary(:,10) = mfe(:,1);
+summary(:,11) = std_gc(:,1);
+summary(:,12) = std_mfe(:,1);
+summary(:,13) = gc3_corr_value(:,1);
+summary(:,14) = gc3_corr_pvalue(:,1);
+summary(:,15) = gc12_corr_value(:,1);
+summary(:,16) = gc12_corr_pvalue(:,1);
+
 
