407a86b4b8cd67f25b119318ad0d47cc9215820e
Yongtao Ye <53943996+ytye2010@users.noreply.github.com>
2025-09-26 17:28:34 +0800

    Delete Scripts/runRNAfold.sh

--- Scripts/runRNAfold.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd ~/yytao/influenza/data/filter
-gene=PB2
-infile=$gene"_nt.pal2nal"
-outfile=./rnafold/$gene"_rnafold.txt"
-
-~/yytao/package/ViennaRNA-2.6.4/src/bin/RNAfold -j20 --MEA --noPS $infile > $outfile
